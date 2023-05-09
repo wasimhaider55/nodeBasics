0e8de357e182f844a511fd0cc319e91e267969c1
wasim Haider55 <wasimhaider14379@gmail.com>
2023-05-09 13:08:57 +0500

    some nodejs basics from zino trust academy

---
+++ app.js
@@ -0,0 +1,16 @@
+const greet = (name) => {
+    console.log(name)
+}
+
+// greet("wasim Haider");
+// greet("asim haider");
+
+
+
+// globalThis.setInterval(() => {
+//     console.log("mern stack course");
+// },2000)
+
+
+// console.log(__dirname);
+// console.log(__filename);
