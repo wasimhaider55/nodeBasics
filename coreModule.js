62f9394c5d7557e0b8087b406faed37f00610dcf
wasim Haider55 <wasimhaider14379@gmail.com>
2023-05-10 11:44:06 +0500

    core module

---
+++ coreModule.js
@@ -0,0 +1,8 @@
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const url = require("url");
+
+// console.log(os);
+// console.log(os.platform());
+// console.log(path);
